cbade8034d9e609b1c0b091c1267b579bc2235c5
James McKay <james.mckay@trausteknik.com>
2021-12-22 12:45:16 +1100

    Add files via upload

---
+++ QueryResultsToCSV.user.js
@@ -0,0 +1,62 @@
+// ==UserScript==
+// @name         QueryResultsToCSV
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Save SF Dev console query results to a csv file. Fairly dumb, won't handle objects.
+// @author       James McKay
+// @match        https://*.my.salesforce.com/_ui/common/apex/debug/ApexCSIPage
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function saveAs(uri, filename) {
+        var link = document.createElement('a');
+        if (typeof link.download === 'string') {
+            link.href = uri;
+            link.download = filename;
+
+            //Firefox requires the link to be in the body
+            document.body.appendChild(link);
+
+            //simulate click
+            link.click();
+
+            //remove the link when done
+            document.body.removeChild(link);
+        } else {
+            window.open(uri);
+        }
+    }
+
+
+    let fDoStuff = function() {
+        document.querySelectorAll('.x-panel .x-panel-body .x-panel.x-grid .x-toolbar > div > div').forEach(function(el){
+            if(el.hasCSVButton)
+            {
+                return;
+            }
+            el.hasCSVButton = true;
+
+            let newEl = document.createElement('div');
+            newEl.class = 'x-btn x-box-item x-toolbar-item x-btn-default-toolbar-small x-noicon x-btn-noicon x-btn-default-toolbar-small-noicon';
+            newEl.style.borderWidth = '1px';
+            newEl.style.position = 'absolute';
+            newEl.style.left = '400px';
+            newEl.style.margin = 0;
+            newEl.style.top = 0;
+
+            let newButton = document.createElement('button');
+            newButton.type = "button";
+            newButton.addEventListener('click', function(ev) {
+                (function() { let lineArray = []; Array.from(document.querySelectorAll('#editors-body > .x-panel:last-child > .x-panel-body-default .x-grid-view table > tbody > tr.x-grid-row')).map(function(el){ return (Array.from(el.querySelectorAll('td')).map(function(d){ return d.innerText; }));}).forEach(function(a,i){ let line = a.join(','); lineArray.push(i == 0 ? "data:text/csv;charset=utf-8," + line : line) }); let s = lineArray.join('\n'); saveAs(s, 'results-' + (new Date()).toISOString() + '.csv'); }())
+            });
+            newButton.innerText = 'Get CSV';
+
+            newEl.appendChild(newButton);
+            el.appendChild(newEl);
+        })
+    };
+    setInterval(fDoStuff, 5000);
+})();
